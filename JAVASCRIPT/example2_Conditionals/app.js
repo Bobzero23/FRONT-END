ac8c8e8a658c69982f4952a9ca75f7cdbe1ccfc0
Bobzero <rajabu.mustafa.00@gmail.com>
2023-04-04 21:29:57 +0300

    something

---
+++ JAVASCRIPT/example2_Conditionals/app.js
@@ -0,0 +1,12 @@
+let randomNumber = Math.random()
+if (randomNumber < 0.5) {
+    console.log("Your random number is less than 0.5!!");
+    console.log(randomNumber);
+}
+
+const dayOfWeek = "Monday";
+if (dayOfWeek === "Monday") {
+    console.log("I hate Mondays");
+} else if (dayOfWeek === "Saturday") {
+    console.log("Yeah I love saturadays");
+}
