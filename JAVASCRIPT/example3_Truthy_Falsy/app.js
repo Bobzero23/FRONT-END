816844c035a379378ee90adab2de3bf18ca9d04e
Bobzero <rajabu.mustafa.00@gmail.com>
2023-04-07 22:07:26 +0300

    Truthy and Falsy

---
+++ JAVASCRIPT/example3_Truthy_Falsy/app.js
@@ -0,0 +1,33 @@
+// const userInput = prompt("Enter something");
+
+/**THESE ARE THE FALSY VALUUES */
+
+// if (0) {
+//     console.log("TRUTHY");
+// } else {
+//     console.log("FALSY");
+// }
+
+// if (false) {
+//     console.log("TRUTHY");
+// } else {
+//     console.log("FALSY");
+// }
+
+// if (undefined) {
+//     console.log("TRUTHY");
+// } else {
+//     console.log("FALSY");
+// }
+
+// if (NaN) {
+//     console.log("TRUTHY");
+// } else {
+//     console.log("FALSY");
+// }
+
+// if (userInput) {
+//     console.log("TRUTHY");
+// } else {
+//     console.log("FALSY");
+// }
