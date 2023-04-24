3a03896c15d06dc3466e84984fa490ae62665731
Bobzero <rajabu.mustafa.00@gmail.com>
2023-04-24 22:11:16 +0300

    switch case implementation completed

---
+++ JAVASCRIPT/example5_Switch/script.js
@@ -0,0 +1,20 @@
+const day = 5;
+switch (day) {
+    case 1:
+        console.log("It's Monday");
+        break;
+    case 2:
+        console.log("It's Teusday");
+        break;
+    case 3:
+        console.log("It's Wednesday");
+        break;
+    case 4:
+        console.log("It's Thursda");
+        break;
+    case 5:
+        console.log("It's Friday");
+        break;
+    default:
+        console.log("It's Weekend");
+}
