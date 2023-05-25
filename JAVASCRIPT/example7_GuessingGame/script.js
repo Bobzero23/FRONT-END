d76201e5fc7e8d54b8e0c6b3a3eff2220f32265a
Bobzero <rajabu.mustafa.00@gmail.com>
2023-05-25 13:33:56 +0300

    guessing game implemented successfully

--- JAVASCRIPT/example7_GuessingGame/script.js
+++ JAVASCRIPT/example7_GuessingGame/script.js
@@ -4,6 +4,30 @@ while (!maximumNumber) {
     maximumNumber = parseInt(prompt("Enter a valid Number!!"));
 }
 
-const tragetNumber = Math.floor(Math.random() * maximumNumber) + 1;
+const targetNumber = Math.floor(Math.random() * maximumNumber) + 1;
 
-let guess = parseInt(prompt("Enter a valid Number!!"));
+let guess = prompt("Enter your guess!! (Type q to quit the game)");
+
+let numberOfGuess = 1;
+
+while (parseInt(guess) != targetNumber) {
+    if (guess === "q") break;
+
+    guess = ParseInt(guess);
+    if (guess > targetNumber) {
+        guess = prompt("Too high.. enter a new guess!!");
+        numberOfGuess++;
+    } else if (guess < targetNumber) {
+        numberOfGuess++;
+        guess = prompt("Too low.. enter a new guess!!");
+    }esle {
+        guess = ParseInt(prompt("Invalid guess, Please a number or q to quit"));
+    }
+}
+
+if (guess === "q") {
+    console.log("OK, YOU QUIT");
+} else {
+    console.log("CONGRATAS... YOU WIN!!");
+    console.log(`You got it, took you ${numberOfGuess} guesses`);
+}
