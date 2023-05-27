5d3c282d3272680bb455f4f262f615710be42139
Bobzero <rajabu.mustafa.00@gmail.com>
2023-05-27 13:41:58 +0300

    setTimeout and setInterval methods

---
+++ JAVASCRIPT/exmaple11_setTimeout_setInterval/script.js
@@ -0,0 +1,10 @@
+/**This is how you create a setTimout function */
+
+// setTimeout(() => {
+//     alert("It has been three seconds")
+// }, 3000);
+
+
+const intervalId = setInterval(() => {
+    console.log("That is two seconds")
+}, 2000);
