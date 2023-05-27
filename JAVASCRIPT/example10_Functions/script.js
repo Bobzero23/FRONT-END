9514fc475e80bb0defc76b0cc9625e431f70c038
Bobzero <rajabu.mustafa.00@gmail.com>
2023-05-27 13:24:16 +0300

    functions is Js

---
+++ JAVASCRIPT/example10_Functions/script.js
@@ -0,0 +1,16 @@
+//this is how you create a function expression 
+const add = function (x, y) {
+    return x + y;
+}
+
+/**This is how you create an arrow function */
+const square = (x) => {
+    return x ** 2
+}
+
+/**This is how you create a map function */
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const multiplyByTen = numbers.map(function (number) {
+    return number * 10;
+});
+
