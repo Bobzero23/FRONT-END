3ab49510afd97cd93528325ea15091ab63dfb331
Bobzero <rajabu.mustafa.00@gmail.com>
2023-06-03 03:10:10 +0300

    score keeper completed successfully

--- Projects/TennisScoreKeeper/app.js
+++ Projects/TennisScoreKeeper/app.js
@@ -5,47 +5,48 @@ const plyr2span = document.querySelector(".plyr2-span");
 const rstBtn = document.querySelector(".rst-btn");
 const plyTo = document.querySelector(".plyTo");
 
-let plyr1Curr = parseInt(plyr1span.innerText);
-let plyr2Curr = parseInt(plyr2span.innerText);
+let p1Score = 0;
+let p2Score = 0;
+let isGameOver = false;
+let winningScore = 3;
 
-const winner = () => {
-    plyr1Curr = parseInt(plyr1span.innerText);
-    plyr2Curr = parseInt(plyr2span.innerText);
-
-    if (plyr1Curr > plyr2Curr) {
-        plyr1span.style.color = "green";
-        plyr2span.style.color = "red";
-    } else if (plyr1Curr < plyr2Curr) {
-        plyr1span.style.color = "red";
-        plyr2span.style.color = "green";
-    } else {
-        plyr1span.style.color = "blue";
-        plyr2span.style.color = "blue";
-        alert("DRAW!!!");
+plyrIncr1.addEventListener("click", function () {
+    if (!isGameOver) {
+        p1Score += 1;
+        if (p1Score === winningScore) {
+            plyr1span.classList.add("winner");
+            plyr2span.classList.add("loser");
+            isGameOver = true;
+        }
+        plyr1span.textContent = p1Score;
     }
+});
+
+plyrIncr2.addEventListener("click", function () {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            plyr1span.classList.add("loser");
+            plyr2span.classList.add("winner");
+            isGameOver = true;
+        }
+        plyr2span.textContent = p2Score;
+    }
+});
+
+plyTo.addEventListener("change", function () {
+    winningScore = parseInt(this.value);
+    reset();
+});
+
+rstBtn.addEventListener("click", reset);
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    plyr1span.textContent = 0;
+    plyr2span.textContent = 0;
+    plyr1span.classList.remove("loser", "winner");
+    plyr2span.classList.remove("winner", "loser");
 }
-
-plyrIncr1.addEventListener("click", () => {
-    plyr1Curr = parseInt(plyr1span.innerText);
-
-    if (plyr1Curr + plyr2Curr == plyTo.value) {
-        winner();
-    }
-
-    plyr1span.innerText = `${plyr1Curr + 1}`;
-});
-
-plyrIncr2.addEventListener("click", () => {
-    plyr2Curr = parseInt(plyr2span.innerText);
-
-    if (plyr1Curr + plyr2Curr == plyTo.value) {
-        winner();
-    }
-
-    plyr2span.innerText = `${plyr2Curr + 1}`;
-});
-
-rstBtn.addEventListener("click", () => {
-    plyr1span.innerText = '0';
-    plyr2span.innerText = '0';
-});
