3cb04f0645bb144fc9fd22b89ce6ad2640e6b605
Bobzero <rajabu.mustafa.00@gmail.com>
2023-06-05 19:57:58 +0300

    completed implementing axios

--- JAVASCRIPT/example18_Axios/app.js
+++ JAVASCRIPT/example18_Axios/app.js
@@ -5,15 +5,27 @@
 //     });
 
 
-const getStarwar = async (id) => {
-    try {
-        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-        console.log(res.data);
-    } catch (error) {
-        console.log("ERROR", error)
-    }
+
+/**INTRO TO AXIOS */
+// const getStarwar = async (id) => {
+//     try {
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//         console.log(res.data);
+//     } catch (error) {
+//         console.log("ERROR", error)
+//     }
+// }
+
+// getStarwar(1);
+// getStarwar(5);
+// getStarwar(10);
+
+
+/**DEALING WITH HEADERS */
+const getDadJoke = async () => {
+    const config = { headers: { accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    console.log(res.data.joke);
 }
 
-getStarwar(1);
-getStarwar(5);
-getStarwar(10);
+getDadJoke();
