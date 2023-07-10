c8e362dd3a9b9188cc11b96a0a3683263c95de5b
Bobzero <rajabu.mustafa.00@gmail.com>
2023-07-10 12:15:03 +0300

    user register page modifications

--- Projects/BookStore/register_user.js
+++ Projects/BookStore/register_user.js
@@ -3,5 +3,13 @@ const form = document.querySelector("#myForm");
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    alert("it is working")
+    const formData = new FormData(this);
+
+    axios.post("http://localhost:8081/addNewUser", Object.fromEntries(formData))
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error.data);
+        })
 });
