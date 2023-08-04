169716a4b5c86ad4a369f6c23a8f548b19443344
Bobzero <rajabu.mustafa.00@gmail.com>
2023-08-04 12:35:14 +0300

    adding id to every array element

--- REACT/state-demo/src/EmojiClicker.jsx
+++ REACT/state-demo/src/EmojiClicker.jsx
@@ -1,22 +1,22 @@
-
+import { v4 as uuid } from "uuid";
 
 import { useState } from "react";
 
 export default function EmojiClicker() {
-    const [emojis, setEmojis] = useState(["😂"]);
+    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😂" }]);
 
     const addEmoji = () => {
         /**This is just a normal way of doing it but not a better way */
         // setEmojis([...emojis, "😘"]);
 
         /**This is a better way on how to do it */
-        setEmojis((oldEmojis) => [...oldEmojis, "😘"]);
+        setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: "😂" }]);
     }
 
     return (
         <div>
             {emojis.map((e) => (
-                <span style={{ fontSize: "4rem" }}>{e}</span>
+                <span key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>
             ))}
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
