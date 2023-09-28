73f74d3523edf408a03ae6f07f4a015cf34b47da
Bobzero <rajabu.mustafa.00@gmail.com>
2023-09-28 17:13:40 +0300

    fixed the bug, now can see the images

--- Projects/Blog/app.js
+++ Projects/Blog/app.js
@@ -1,8 +1,7 @@
 const formEl = document.querySelector(".post-form");
 
-
-formEl.addEventListener('submit', function(evetnt) {
-    evetnt.preventDefault();
+formEl.addEventListener('submit', function(event) {
+    event.preventDefault();
 
     const imageEl = document.querySelector("#image").files[0];
     const titleEl = document.querySelector("#title").value;
@@ -16,61 +15,56 @@ formEl.addEventListener('submit', function(evetnt) {
     axios.post('http://localhost:1001/posts', formData)
     .then((response) => {
         console.log("post created: ", response.data);
+        location.reload();
     })
-    .catch((error) =>  {
+    .catch((error) => {
         console.error("Error", error);
-    })
-
-    location.reload();
+    });
 });
-
-
 
 // Function to fetch and display posts
 function displayPosts() {
     axios.get('http://localhost:1001/posts') // Replace with your actual GET endpoint
-      .then((response) => {
+    .then((response) => {
         console.log(response.data);
         const cardContainer = document.getElementById('card-container');
         cardContainer.innerHTML = ''; // Clear previous content
-  
+
         // Loop through the posts and create cards for each
         response.data.forEach((post) => {
-          // Create card elements
-          const card = document.createElement('div');
-          card.className = 'card';
-  
-          const cardImg = document.createElement('img');
-          cardImg.src = post.image; // Replace with the actual image URL
-          cardImg.className = 'card-img-top';
-  
-          const cardBody = document.createElement('div');
-          cardBody.className = 'card-body';
-  
-          const cardTitle = document.createElement('h5');
-          cardTitle.className = 'card-title';
-          cardTitle.textContent = post.title;
-  
-          const cardText = document.createElement('p');
-          cardText.className = 'card-text';
-          cardText.textContent = post.content;
-  
-          // Append elements to the card
-          cardBody.appendChild(cardTitle);
-          cardBody.appendChild(cardText);
-          card.appendChild(cardImg);
-          card.appendChild(cardBody);
-  
-          // Append the card to the container
-          cardContainer.appendChild(card);
+            // Create card elements
+            const card = document.createElement('div');
+            card.className = 'card';
 
+            const cardImg = document.createElement('img');
+            cardImg.src = `data:image/jpeg;base64,${post.image}`; // Assuming your response contains base64-encoded image data
+            cardImg.className = 'card-img-top';
+
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+            const cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title';
+            cardTitle.textContent = post.title;
+
+            const cardText = document.createElement('p');
+            cardText.className = 'card-text';
+            cardText.textContent = post.content;
+
+            // Append elements to the card
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText);
+            card.appendChild(cardImg);
+            card.appendChild(cardBody);
+
+            // Append the card to the container
+            cardContainer.appendChild(card);
         });
-      })
-      .catch((error) => {
+    })
+    .catch((error) => {
         console.error('Error:', error);
-      });
-  }
-  
-  // Call the function to initially display posts
-  displayPosts();
-  
+    });
+}
+
+// Call the function to initially display posts
+displayPosts();
