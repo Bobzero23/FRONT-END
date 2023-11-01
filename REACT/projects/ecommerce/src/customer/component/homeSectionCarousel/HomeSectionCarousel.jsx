bd76addf07f670fd58330572a891ae3bbed62fda
Bobzero23 <rajabu.mustafa.00@gmail.com>
2023-11-02 01:25:32 +0300

    second carousel completed

--- REACT/projects/ecommerce/src/customer/component/homeSectionCarousel/HomeSectionCarousel.jsx
+++ REACT/projects/ecommerce/src/customer/component/homeSectionCarousel/HomeSectionCarousel.jsx
@@ -6,17 +6,22 @@ import { mens_kurta } from "../../../data/mens_kurta";
 import Button from "@mui/material/Button";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 
-function HomeSectionCarousel() {
+function HomeSectionCarousel({ data, sectionName }) {
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5.5 },
   };
 
-  const items = mens_kurta.map((item) => <HomeSectionCard product={item} />);
+  const items = mens_kurta
+    .slice(0, 10)
+    .map((item) => <HomeSectionCard product={item} />);
 
   // Create a reference to the carousel instance
   const [carousel, setCarousel] = useState(null);
+
+  // Create a state to store the current active index of the carousel
+  const [activeIndex, setActiveIndex] = useState(0);
 
   // Define the functions to handle the button clicks
   const handlePrev = () => {
@@ -27,8 +32,21 @@ function HomeSectionCarousel() {
     carousel.slideNext();
   };
 
+  // Define a function to handle the slide change event
+  const handleSlideChange = (event) => {
+    // Update the active index state with the new index from the event object
+    setActiveIndex(event.item);
+  };
+
+  // Get the number of items per slide based on the window width
+  const itemsPerSlide =
+    window.innerWidth < 720 ? 1 : window.innerWidth < 1024 ? 3 : 5.5;
+
   return (
     <div className="relative border">
+      <h2 className="text-2xl font-extrabold text-gray-800 py-2 px-2">
+        {sectionName}
+      </h2>
       <div className="relative p-5 ">
         <AliceCarousel
           // Pass the reference to the carousel instance
@@ -36,47 +54,55 @@ function HomeSectionCarousel() {
           items={items}
           responsive={responsive}
           disableButtonsControls
-          autoPlay
-          autoPlayInterval={3000}
           disableDotsControls
+          // Add the onSlideChange handler to update the active index state before the slide changes
+          onSlideChange={handleSlideChange}
         />
         {/* Add the buttons to the carousel container */}
-        <Button
-          variant="contained"
-          className="z-50 bg-white"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            left: "0rem",
-            transform: "translateX(-50%) rotate(-90deg)",
-            bgcolor: "white",
-          }}
-          aria-label="next"
-          // Add the onClick handler for the prev button
-          onClick={handlePrev}
-        >
-          <KeyboardArrowLeftIcon
-            sx={{ transform: "rotate(90deg)", color: "black" }}
-          />
-        </Button>
-        <Button
-          variant="contained"
-          className="z-50 bg-white"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            right: "0rem",
-            transform: "translateX(50%) rotate(-90deg)",
-            bgcolor: "white",
-          }}
-          aria-label="next"
-          // Add the onClick handler for the next button
-          onClick={handleNext}
-        >
-          <KeyboardArrowLeftIcon
-            sx={{ transform: "rotate(-90deg)", color: "black" }}
-          />
-        </Button>
+        {/* Show the prev button only if the active index is not zero */}
+        {activeIndex !== 0 && (
+          <Button
+            variant="contained"
+            className="z-50 bg-white"
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+            // Add the onClick handler for the prev button
+            onClick={handlePrev}
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+
+        {/* Show the next button only if the active index is not at the end of the items array */}
+        {/* Use Math.ceil instead of Math.floor to round up the number of items per slide */}
+        {activeIndex !== Math.ceil(items.length - itemsPerSlide) && (
+          <Button
+            variant="contained"
+            className="z-50 bg-white"
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+            // Add the onClick handler for the next button
+            onClick={handleNext}
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(-90deg)", color: "black" }}
+            />
+          </Button>
+        )}
       </div>
     </div>
   );
--- REACT/projects/ecommerce/src/customer/component/pages/homePage/HomePage.jsx
+++ REACT/projects/ecommerce/src/customer/component/pages/homePage/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import MainCarousel from "../../HomeCarousel/MainCarousel";
 import HomeSectionCarousel from "../../homeSectionCarousel/HomeSectionCarousel";
+import { mens_kurta } from "../../../../data/mens_kurta";
 
 function HomePage() {
   return (
     <div>
       <MainCarousel />
       <div className="space-y-10 py-10 flex flex-col justify-center px-5 lg:px-10 border">
-        <HomeSectionCarousel />
+        <HomeSectionCarousel data={mens_kurta} sectionName={"Mens Kurta"} />
       </div>
     </div>
   );
