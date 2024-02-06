f35a5d307f545863a80d49aef9ef0f93363b2b02
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-02-06 21:15:58 +0300

    adimn panel drawer ui navigation complete

--- REACT/projects/ecommerce/src/admin/components/Admin.jsx
+++ REACT/projects/ecommerce/src/admin/components/Admin.jsx
@@ -1,9 +1,91 @@
-import React from "react";
+import Dashboard from "@mui/icons-material/Dashboard";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import EmailIcon from "@mui/icons-material/Email";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-const menu = [];
+import {
+  Box,
+  CssBaseline,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+
+const menu = [
+  { name: "Dashboard", paht: "/admin", icon: <Dashboard /> },
+  { name: "Products", paht: "/admin/products", icon: <Dashboard /> },
+  { name: "Customers", paht: "/admin/customers", icon: <Dashboard /> },
+  { name: "Orders", paht: "/admin/orders", icon: <Dashboard /> },
+  { name: "AddProducts", paht: "/admin/product/create", icon: <Dashboard /> },
+];
 
 const Admin = () => {
-  return <div>Admin</div>;
+  const theme = useTheme();
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+  const [sideBarVisible, setSideBarVisible] = useState(false);
+  const navigate = useNavigate();
+
+  const drawer = (
+    <Box
+      sx={{
+        overflow: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+      }}
+    >
+      {/* {isLargeScreen && <Toolbar />} */}
+      <List>
+        {menu.map((item, index) => (
+          <ListItem
+            key={item.name}
+            disablePadding
+            onClick={() => navigate(item.path)}
+          >
+            <ListItemButton>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText>{item.name}</ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+      <List>
+        <ListItem disablePadding onClick={() => navigate(item.path)}>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            <ListItemText>Account</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Box sx={{ display: `${isLargeScreen}: "fles" : "block"` }}>
+        <CssBaseline />
+        <Drawer
+          variant="permanent"
+          sx={{ height: "100vh", width: 240, flexShrink: 0 }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </div>
+  );
 };
 
 export default Admin;
--- REACT/projects/ecommerce/src/routers/AdminRouters.jsx
+++ REACT/projects/ecommerce/src/routers/AdminRouters.jsx
@@ -1,74 +1,7 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Admin from "../admin/components/Admin";
-import EmailIcon from "@mui/icons-material/Email";
-import InboxIcon from "@mui/icons-material/Inbox";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import {
-  Box,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  Toolbar,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import { useState } from "react";
-import Dashboard from "@mui/icons-material/Dashboard";
-
-const menu = [
-  { name: "Dashboard", paht: "/admin", icon: <Dashboard /> },
-  { name: "Products", paht: "/admin/products", icon: <Dashboard /> },
-  { name: "Customers", paht: "/admin/customers", icon: <Dashboard /> },
-  { name: "Orders", paht: "/admin/orders", icon: <Dashboard /> },
-  { name: "AddProducts", paht: "/admin/product/create", icon: <Dashboard /> },
-  { name: "", paht: "" },
-  { name: "", paht: "" },
-];
 
 const AdminRouters = () => {
-  const theme = useTheme();
-  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
-  const [sideBarVisible, setSideBarVisible] = useState(false);
-  const navigate = useNavigate();
-
-  const drawer = (
-    <Box
-      sx={{
-        overflow: "auto",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
-    >
-      {isLargeScreen && <Toolbar />}
-      <List>
-        {menu.map((item, index) => (
-          <ListItem
-            key={item.name}
-            disablePadding
-            onClick={() => navigate(item.path)}
-          >
-            <ListItemButton>
-              <ListItemIcon>{item.icon}</ListItemIcon>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-
-      <List>
-        <ListItem disablePadding onClick={() => navigate(item.path)}>
-          <ListItemButton>
-            <ListItemIcon>
-              <AccountCircleIcon />
-            </ListItemIcon>
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Box>
-  );
-
   return (
     <div>
       <Routes>
