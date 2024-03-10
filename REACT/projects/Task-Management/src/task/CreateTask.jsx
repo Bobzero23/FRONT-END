22a4a4668dc32bb814a8ae0deaf21665866ffaf2
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-03-10 15:35:41 +0300

    working on create new task form

--- REACT/projects/Task-Management/src/task/CreateTask.jsx
+++ REACT/projects/Task-Management/src/task/CreateTask.jsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import Modal from "@mui/material/Modal";
-import { Grid, TextField } from "@mui/material";
+import { Autocomplete, Button, Grid, TextField } from "@mui/material";
 import { useState } from "react";
 
 const style = {
@@ -12,32 +14,46 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "0px solid black",
+  border: "1px solid white",
   boxShadow: 24,
   p: 4,
 };
 
-const [formData, setFormData] = useState({
-  title: "",
-  image: "",
-  description: "",
-  tage: [],
-  deadline: new Date(),
-});
-
-const handleChange = (event) => {
-  const { name, value } = e.target;
-  setFormData({
-    ...formData,
-    [name]: value,
-  });
-};
-
-const handleTagsChange = (event, value) => {
-  setSelectedTags(value);
-};
+const tags = ["Angular", "React", "Vue", "SpringBoot", "Node", "Python"];
 
 export default function CreateTask({ handleClose, open }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    image: "",
+    description: "",
+    tage: [],
+    deadline: new Date(),
+  });
+  const handleChange = (event) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const [selectedTags, setSelectedTags] = useState();
+
+  const handleTagsChange = (event, value) => {
+    setSelectedTags(value);
+  };
+
+  const handleDeadlineChange = (date) => {
+    setFormData({
+      ...formData,
+      deadline: date,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <Modal
@@ -56,6 +72,53 @@ export default function CreateTask({ handleClose, open }) {
                 value={formData.title}
                 onChange={handleChange}
               />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="image"
+                name="image"
+                fullWidth
+                value={formData.image}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="description"
+                name="description"
+                multiline
+                rows={4}
+                fullWidth
+                value={formData.description}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Autocomplete
+                multiple
+                id="multiple-limit-tags"
+                options={tags}
+                onChange={handleTagsChange}
+                getOptionLabel={(option) => option}
+                renderInput={(params) => (
+                  <TextField label="image" fullWidth {...params} />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateTimePicker
+                  className="w-full"
+                  label="Deadline"
+                  onChange={handleDeadlineChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" fullWidth sx={{ padding: "0.9rem" }}>
+                Create
+              </Button>
             </Grid>
           </Grid>
         </Box>
