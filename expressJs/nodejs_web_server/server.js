848ac7336a52cc340fdb9fc80bb2bd5d98fa6b11
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-03-26 22:00:48 +0300

    redirecting to another endpoint

--- expressJs/nodejs_web_server/server.js
+++ expressJs/nodejs_web_server/server.js
@@ -3,13 +3,19 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
+/**a normal way of sending a file */
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
 });
 
 /**this is another way of doing the same thing above*/
-app.get("^/$new-page(.html)?", (req, res) => {
+app.get("^/$|new-page(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+/**redirecting to a new page which is the endpoint above*/
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
