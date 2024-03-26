658403d3121288ed8baa0d9ff0bc023d5771e677
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-03-26 15:32:55 +0300

    working with file system in node:- Asynchronous and Synchronous

--- node/boilerplate.js
+++ node/boilerplate.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 console.log(fs);
 
-// fs.mkdir("", { recursive: true }, (err) => {
+/**this file system method is asynchronous*/
+// fs.mkdir("Dogs", { recursive: true }, (err) => {
+//   console.log("INSIDE THE CALL BACK");
 //   if (err) throw err;
 // });
+// console.log("AFTER THE CALL BACK");
+
+/**this file system method is synchronous*/
+fs.mkdirSync("Cats");
