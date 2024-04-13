ef3e7c39ac3363daccd121e169a26284e80739f6
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-04-13 15:38:47 +0300

    cleaning the code:- migrating to mvc pattern

--- node/MyFirstNode.js
+++ node/MyFirstNode.js
@@ -1,9 +1,19 @@
 var http = require("http");
+var fs = require("fs");
 
 http
   .createServer(function (req, res) {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
+    //reading the url
+    res.write(req.url);
     res.end("Hello Bobzero");
   })
   .listen(8080);
+
+const fileName = process.argv[2] || "fileSystem";
+fs.mkdirSync(fileName);
+
+fs.writeFileSync(`${fileName}/index.html`, "");
+fs.writeFileSync(`${fileName}/style.css`, "");
+fs.writeFileSync(`${fileName}/app.js`, "");
