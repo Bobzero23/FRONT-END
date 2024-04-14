df6838fd51fdfd490962f258f43cd5ea7b1db940
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-04-14 09:44:39 +0300

    creating jwts, jwt lifetime, access and refresh token

--- expressJs/nodejs_web_server/controllers/authController.js
+++ expressJs/nodejs_web_server/controllers/authController.js
@@ -7,6 +7,11 @@ const usersDB = {
 
 const bcrypt = require("bcrypt");
 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
+
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
   if (!user || !pwd) {
@@ -19,7 +24,34 @@ const handleLogin = async (req, res) => {
   if (!foundUser) return res.sendStatus(401); //unauthorized
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
-    //TO-DO :- create JWT
+    //creating jwts
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    //saving refresh token with current user
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    }); //one day lifetime httpOnly so js can't access it
     res.json({ success: `User ${user} is logged in!` });
   } else {
     res.sendStatus(401);
