3d9cb625ed74bb12aa411120c8e0b6acbb4ab5a7
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-04-18 22:04:44 +0300

    logout controller implementation complete

--- expressJs/nodejs_web_server/controllers/logoutController.js
+++ expressJs/nodejs_web_server/controllers/logoutController.js
@@ -9,7 +9,7 @@ const fsPromises = require("fs").promises;
 const { Person } = require("@mui/icons-material");
 const path = require("path");
 
-const handleLogout = (req, res) => {
+const handleLogout = async (req, res) => {
   //TODO - On client also delete the accessToken
 
   const cookies = req.cookies;
@@ -32,6 +32,14 @@ const handleLogout = (req, res) => {
   );
   const currentUser = { ...foundUser, refreshToken: "" };
   usersDB.setUsers([...otherUsers, currentUser]);
+
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "model", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+
+  res.clearCookie("jwt", { httpOnly: true }); // "secure: true" only serves on https
+  res.sendStatus(409);
 };
 
 module.exports = { handleLogout };
