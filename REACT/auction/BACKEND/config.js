1e70e820b2c0e4939be7c65e7369f1e3844f17e6
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-10 19:14:53 +0300

    mongoDB init commit

--- REACT/auction/BACKEND/config.js
+++ REACT/auction/BACKEND/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDBURL = `mongodb+srv://root:root@bobzero.vqoi7td.mongodb.net/Auction?retryWrites=true&w=majority&appName=Bobzero`;
--- REACT/auction/BACKEND/index.js
+++ REACT/auction/BACKEND/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
