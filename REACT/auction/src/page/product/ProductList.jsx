717c181e9282edb363d109c6d37118bfeb397221
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-10 16:48:12 +0300

    adding a scroll bar in product page to keep the product button and the user balance visible all the time

--- REACT/auction/src/page/product/ProductList.jsx
+++ REACT/auction/src/page/product/ProductList.jsx
@@ -3,22 +3,24 @@ import Product from "./Product";
 import { Button } from "@mui/material";
 
 // demo list
-const list = [1, 1, 1, 1, 1, 1, 1, 1];
+const list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 // demo admin
 const admin = false;
+const user = true;
 
 const ProductList = () => {
   return (
     <div className="flex flex-col space-y-3 items-center justify-center p-5">
-      <div className="flex gap-1 flex-wrap justify-center items-center">
+      <div className="flex gap-1 flex-wrap justify-center items-center overflow-y-auto h-[calc(100vh-100px)]">
         {list.map((item) => (
           <Product />
         ))}
       </div>
       <div>
-        {admin ? (
+        {user ? (
           <Button variant="outlined">
-            YOUR CURRENT BALANCE: <span> 500,000</span>
+            YOUR CURRENT BALANCE:{" "}
+            <span className="ml-1 font-bold"> 500,000</span>
           </Button>
         ) : (
           <Button variant="contained">ADD PRODUCT</Button>
