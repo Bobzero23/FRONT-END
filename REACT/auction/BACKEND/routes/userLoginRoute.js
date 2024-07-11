392f1e758c80cbf78e6d6c60abbf79d147b000b0
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-11 17:30:02 +0300

    user log in implementation

--- REACT/auction/BACKEND/routes/userLoginRoute.js
+++ REACT/auction/BACKEND/routes/userLoginRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/userModel";
+import bcrypt from "bcrypt";
 import Joi from "joi";
 import passwordComplexity from "joi-password-complexity";
 
@@ -7,5 +8,42 @@ export const router = express.Router();
 
 router.post("/", async (response, request) => {
   try {
-  } catch (error) {}
+    const { error } = validate(request.body);
+
+    if (error) {
+      return response.status(400).send({ message: error.details[0].message });
+    }
+
+    const user = await User.findOne({ email: request.body.email });
+
+    if (!user) {
+      return response
+        .status(401)
+        .send({ message: "Invalid email or password" });
+    }
+
+    const validPassword = await bcrypt.compare(
+      request.body.password,
+      user.password
+    );
+
+    if (!validPassword) {
+      return response.status(401).send({ message: "Passowrd is incorrect" });
+    }
+
+    const token = user.generateAuthToken();
+
+    return response.status(200).send({ message: "Logged in successfully!" });
+  } catch (error) {
+    console.log("Error happend while authenticating the user");
+  }
 });
+
+const validate = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().label("Email"),
+    password: Joi.string().required().label("Password"),
+  });
+
+  return schema.validate(data);
+};
