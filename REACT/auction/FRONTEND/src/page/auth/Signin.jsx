b1527a04cdedc5d3b368a857ad2087d521394974
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-14 15:24:01 +0300

    mechanism for login a user

--- REACT/auction/FRONTEND/src/page/auth/Signin.jsx
+++ REACT/auction/FRONTEND/src/page/auth/Signin.jsx
@@ -1,27 +1,71 @@
 import { Button, TextField } from "@mui/material";
-import React from "react";
+import { useState } from "react";
+import { login } from "../../services/apiServices";
+import Spinner from "../../components/Spinner";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    const response = await login(formData);
+    setIsLoading(false);
+    if (response.status === 200) {
+      toast.success(response.message);
+      navigate("/");
+    } else {
+      toast.error(response.message);
+    }
+  };
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div>
-        <h1 className="text-lg font-bold text-center pb-8">SIGN IN</h1>
-        <form className="space-y-3">
-          <TextField fullWidth name="username" label="username or email" />
-          <TextField
-            fullWidth
-            name="password"
-            label="password"
-            type="password"
-          />
+    <>
+      <Spinner isLoading={isLoading} />
+      <div className="flex items-center justify-center h-screen">
+        <div>
+          <h1 className="text-lg font-bold text-center pb-8">SIGN IN</h1>
+          <form className="space-y-3" onSubmit={handleSubmit}>
+            <TextField
+              onChange={handleChange}
+              fullWidth
+              name="email"
+              label="email"
+            />
+            <TextField
+              onChange={handleChange}
+              fullWidth
+              name="password"
+              label="password"
+              type="password"
+            />
+            <div>
+              <Button fullWidth type="submit" variant="contained">
+                SIGNIN
+              </Button>
+            </div>
+          </form>
           <div>
-            <Button fullWidth type="submit" variant="contained">
-              SIGNIN
-            </Button>
+            <span>Already have an account? </span>
+            <Button onClick={() => navigate("/signup")}>Sign Up </Button>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
