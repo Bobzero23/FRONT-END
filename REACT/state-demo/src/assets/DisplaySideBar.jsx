7670ddbeaf098cc420c61120ebc59dc506da5c0a
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-16 11:16:11 +0300

    fetching demo data from json and displaying them diynamically on a click

--- REACT/state-demo/src/assets/DisplaySideBar.jsx
+++ REACT/state-demo/src/assets/DisplaySideBar.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Stack from "react-bootstrap/Stack";
 import Button from "react-bootstrap/Button";
 
 const DisplaySideBar = () => {
-  const [display, setdisplay] = useState("products");
+  const [display, setdisplay] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${display}`)
+      .then((response) => response.json())
+      .then((response) => setItems(response));
+  });
+
   return (
     <Stack className="" direction="vertical" gap={2}>
       <Stack direction="horizontal" gap={2}>
-        <Button onClick={() => setdisplay("products")} variant="success">
-          produts
+        <Button onClick={() => setdisplay("posts")} variant="success">
+          posts
         </Button>
-        <Button onClick={() => setdisplay("cards")}>cards</Button>
-        <Button onClick={() => setdisplay("checkout")} variant="danger">
-          checkout
+        <Button onClick={() => setdisplay("users")}>users</Button>
+        <Button onClick={() => setdisplay("comments")} variant="danger">
+          comments
         </Button>
       </Stack>
       <p>Right now you are displaying: {display} </p>
+      <Stack direction="vertical">
+        {items.map((item, index) => (
+          <div className="border-bottom" key={index}>
+            {JSON.stringify(item)}
+          </div>
+        ))}
+      </Stack>
     </Stack>
   );
 };
