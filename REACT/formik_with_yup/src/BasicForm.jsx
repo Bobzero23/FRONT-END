e5f3327d7344c6f105e53833c497aab2ee36225d
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-18 15:20:20 +0300

    creating a form where the input turns red when yo click out without satisfying the validation

--- REACT/formik_with_yup/src/BasicForm.jsx
+++ REACT/formik_with_yup/src/BasicForm.jsx
@@ -1,19 +1,30 @@
 import { useFormik } from "formik";
 import React from "react";
+import { basicFormSchema } from "./schemas/BasicFormSchema";
+
+const onSubmit = () => {
+  console.log("submitted");
+};
 
 const BasicForm = () => {
-  const { values, handleBlur, handleChange } = useFormik({
-    initialValues: {
-      email: "",
-      age: "",
-      password: "",
-      confirmPassword: "",
-    },
-  });
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+        age: "",
+        password: "",
+        confirmPassword: "",
+      },
+      validationSchema: basicFormSchema,
+      onSubmit,
+    });
+
+  console.log(errors);
 
   return (
     <div>
       <form
+        onSubmit={handleSubmit}
         autoComplete="off"
         style={{
           display: "flex",
@@ -31,6 +42,7 @@ const BasicForm = () => {
           type="email"
           id="email"
           placeholder="Enter your email"
+          className={errors.email && touched.email ? "input-error" : ""}
         />
         <label htmlFor="age">age</label>
         <input
@@ -41,15 +53,18 @@ const BasicForm = () => {
           type="number"
           id="age"
           placeholder="Enter your age"
+          className={errors.age && touched.age ? "input-error" : ""}
         />
         <label htmlFor="password">password</label>
         <input
           style={{ height: "30px" }}
           value={values.password}
           onChange={handleChange}
+          onBlur={handleBlur}
           type="password"
           id="password"
           placeholder="Enter your password"
+          className={errors.password && touched.password ? "input-error" : ""}
         />
         <label htmlFor="confirmPassword">confirm password</label>
         <input
@@ -60,6 +75,11 @@ const BasicForm = () => {
           type="password"
           id="confirmPassword"
           placeholder="Confrim your password"
+          className={
+            errors.confirmPassword && touched.confirmPassword
+              ? "input-error"
+              : ""
+          }
         />
         <button type="submit">Submit</button>
       </form>
