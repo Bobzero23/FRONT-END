9976eb8825de8518e9c1815da2d75b55ca632a04
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-19 12:15:16 +0300

    creating a component wich receives some props and render the component in the application

---
+++ REACT/prop_types/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Component from "./Component";
+
+function App() {
+  return (
+    <>
+      <Component name="Bobzero" age={40} />
+    </>
+  );
+}
+
+export default App;
---
+++ REACT/prop_types/src/Component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Component = ({ name, age }) => {
+  return `In 5 years ${name} will be ${age}`;
+};
+
+export default Component;
