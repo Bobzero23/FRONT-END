a4857a3d044449e30179128b336fae079b35e9bb
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-19 12:29:23 +0300

    intialize the prop types and validating them

--- REACT/prop_types/src/Component.jsx
+++ REACT/prop_types/src/Component.jsx
@@ -4,4 +4,9 @@ const Component = ({ name, age }) => {
   return `In 5 years ${name} will be ${age}`;
 };
 
+Component.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
+};
+
 export default Component;
