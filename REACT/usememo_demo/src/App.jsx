43f01dc34994cef68753d100e583f517f200538d
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-20 16:36:43 +0300

    init commit: an app which randers a number field and multiply the entered number but also has a button to change the displayed number value which is multiplied

---
+++ REACT/usememo_demo/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = slowFunction(number);
+
+  const themeStyle = {
+    backGroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+
+  return (
+    <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyle}>{doubleNumber}</div>
+    </div>
+  );
+}
+
+function slowFunction(number) {
+  console.log("Calling slow function");
+  for (let i = 0; i <= 10000000; i++) {}
+  return number * 2;
+}
+
+export default App;
