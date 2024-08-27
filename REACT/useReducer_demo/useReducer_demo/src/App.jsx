c032960788bdc568847eff1228a4dd2628fffdee
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-27 14:21:14 +0300

    updating the state using reducer action type with useReucer react hook

--- REACT/useReducer_demo/useReducer_demo/src/App.jsx
+++ REACT/useReducer_demo/useReducer_demo/src/App.jsx
@@ -2,20 +2,32 @@ import { useReducer, useState } from "react";
 import "./App.css";
 
 function reducer(state, action) {
-  return { count: state.count + 1 };
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
 }
 
 function App() {
   const [state, dispatch] = useReducer(reducer, { count: 0 });
+
   function increment() {
-    dispatch();
+    dispatch({ type: "increment" });
+  }
+
+  function decrement() {
+    dispatch({ type: "decrement" });
   }
 
   return (
     <div>
       <button onClick={increment}>+</button>
       <span>{state.count}</span>
-      <button onClick={() => setCount((prevCount) => prevCount - 1)}>-</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
