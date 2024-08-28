236b2f5a49c8863156b67b2fdd42358ae450fbbf
Rajabu Mustafa Khalfani <recep@iyimakina.com>
2024-08-28 11:34:54 +0300

    init commit: todo app using useReducer react hook

--- REACT/useReducer_demo/useReducer_demo/src/App.jsx
+++ REACT/useReducer_demo/useReducer_demo/src/App.jsx
@@ -1,35 +1,7 @@
-import { useReducer, useState } from "react";
 import "./App.css";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
-}
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-
-  function increment() {
-    dispatch({ type: "increment" });
-  }
-
-  function decrement() {
-    dispatch({ type: "decrement" });
-  }
-
-  return (
-    <div>
-      <button onClick={increment}>+</button>
-      <span>{state.count}</span>
-      <button onClick={decrement}>-</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
