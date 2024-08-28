a1e784d13b8ddeaf24de4a767db38841fc7b3d97
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-28 17:43:43 +0300

    todo app with react hook useReducer form component

--- REACT/useReducer_demo/useReducer_demo/src/App.jsx
+++ REACT/useReducer_demo/useReducer_demo/src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+    setName("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={name}
+          type="text"
+          placeholder="Enter a todo"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
