f03ec6fcc995c732d497292646e85317ffa62680
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-30 23:49:19 +0300

    creating variables

---
+++ TYPESCRIPT/learnings/tutorial.ts
@@ -0,0 +1,8 @@
+let age: number = 5;
+let company: string = "Bobzero";
+let isPublished: boolean = false;
+
+let ids: number[] = [1, 2, 3, 4, 5]
+let names: string[] = ["name1", "name2", "name3"]
+let x: any = "This could be anything"
+let xArr: any[] = ["string", 1, true]
