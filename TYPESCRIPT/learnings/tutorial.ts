7296bd35071bfbd3643a67dd507be32a5fd84f81
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-31 10:00:47 +0300

    implementing a funciton in typescript

--- TYPESCRIPT/learnings/tutorial.ts
+++ TYPESCRIPT/learnings/tutorial.ts
@@ -1,8 +1,15 @@
-let age: number = 5;
-let company: string = "Bobzero";
-let isPublished: boolean = false;
+// let age: number = 5;
+// let company: string = "Bobzero";
+// let isPublished: boolean = false;
 
-let ids: number[] = [1, 2, 3, 4, 5]
-let names: string[] = ["name1", "name2", "name3"]
-let x: any = "This could be anything"
-let xArr: any[] = ["string", 1, true]
+// let ids: number[] = [1, 2, 3, 4, 5]
+// let names: string[] = ["name1", "name2", "name3"]
+// let x: any = "This could be anything"
+// let xArr: any[] = ["string", 1, true]
+
+
+const concatenateValues = (a: string, b: string): string => {
+    return a + b;
+}
+
+console.log(concatenateValues("Hello" , "world"))
