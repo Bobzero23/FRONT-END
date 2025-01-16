22fb73ce2689e8e5bd70c2416a7ea0829acedd9a
Bobzero23 <rajabu.mustafa.00@gmail.com>
2025-01-17 00:12:56 +0300

    visual update, but remove nor clear the user yet

--- REACT/random/src/App.jsx
+++ REACT/random/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import data from './data.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleRemove = (index) => {
+    alert("This is the user index: " + index)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>User List</h1>
+        {
+          data.map((user, index) => {
+            return <li key={index} style={{listStyleType: "none"}}>
+              <h4>{user.firstName} {" "} {user.lastName}</h4> 
+              <button onClick={() => handleRemove(index)}>remove</button>
+            </li>
+          })
+        }
+
+        <button style={{marginTop: "20px"}}>CLEAR</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
