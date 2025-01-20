4b1625e360cda6b36910e7d64fcec124b23a0b11
Rajabu Musatafa Khalfani <recep@iyimakina.com>
2025-01-20 14:14:47 +0300

    refactor: clean up Questions component and improve SingleQuestion mapping

--- REACT/random/src/Questions.jsx
+++ REACT/random/src/Questions.jsx
@@ -1,17 +1,19 @@
-import data from './data';
+import data from "./data";
 
-import React, { useState } from 'react'
-import { SingleQuestion } from './SingleQuestion';
+import React, { useState } from "react";
+import { SingleQuestion } from "./SingleQuestion";
 
 const Questions = () => {
-    const [questions, setQuestions] = useState(data);
-    const [activeId, setActiveId] = useState(null);
+  const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
 
-    return (
-      <div>
-          {questions.map((question)=> <SingleQuestion key={question.id} id={question.id} question={question.question} answer={question.answer} activeId={activeId}/>)}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {questions.map((question) => (
+        <SingleQuestion key={question.id} />
+      ))}
+    </div>
+  );
+};
 
-export default Questions
+export default Questions;
